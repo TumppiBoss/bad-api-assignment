2b970d09d01e5f98b11031ef4f44862cae4773a2
TumppiBoss <65035861+TumppiBoss@users.noreply.github.com>
2021-02-28 17:36:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+console.log(PORT);
+app.listen(PORT, () => console.log('ready for work'));
+app.use(express.static('public'));
+
+app.get('/beanies', async (req, res) => {
+  const api_url = 'https://bad-api-assignment.reaktor.com/v2/products/beanies';
+  const response = await fetch(api_url);
+  const data = await response.json();
+  res.json(data);
+});
+
+app.get('/facemasks', async (req, res) => {
+  const api_url =
+    'https://bad-api-assignment.reaktor.com/v2/products/facemasks';
+  const response = await fetch(api_url);
+  const data = await response.json();
+  res.json(data);
+});
+
+app.get('/gloves', async (req, res) => {
+  const api_url = 'https://bad-api-assignment.reaktor.com/v2/products/gloves';
+  const response = await fetch(api_url);
+  const data = await response.json();
+  res.json(data);
+});
+
+app.get('/availability/:manufacturer', async (req, res) => {
+  const request = req.params;
+  const manufacturer = request.manufacturer;
+  const api_url = `https://bad-api-assignment.reaktor.com/v2/availability/${manufacturer}`;
+  const response = await fetch(api_url);
+  const data = await response.json();
+  res.json(data);
+});
